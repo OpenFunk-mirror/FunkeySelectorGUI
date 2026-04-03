56f9eff86b776dc8398cd156a822a252db814fbf
NanderTGA <nander.roobaert@gmail.com>
2026-04-04 00:54:19 +0200

    chore: rename FunkeySelector to FunkeySelectorGUI internally

---
+++ FunkeySelectorGUI/FunkeysTown.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using FunkeySelectorGUI.UserControls;
+
+namespace FunkeySelectorGUI
+{
+    public partial class FunkeysTown : BasicForm
+    {
+        public FunkeysTown()
+        {
+            InitializeComponent();
+        }
+    }
+}
